340237bf0ad39efa4ae827bca7ad83a08380be56
witawas leesuksomboon <42628023+gunwitawas@users.noreply.github.com>
2020-02-20 17:16:28 +0700

    manage booklist

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,30 +1,102 @@
 const bookSchema = require('../schema')
+const ObjectID = require('mongodb').ObjectID
 
-exports.getBookList = (req, res) => {
-
-    const bookList = [{
-            imageUrl: '1',
-            title: '11',
-            author: '111',
-            description: '1111',
-        },
-        {
-            imageUrl: '2',
-            title: '22',
-            author: '222',
-            description: '2222',
-        },
-        {
-            imageUrl: '3',
-            title: '33',
-            author: '333',
-            description: '3333',
+exports.createBook = (req, res) => {
+    console.log('createBook REQUEST_BODY :', req.body);
+    bookSchema.create(req.body).then((data) => {
+        console.log('RESPONSE_BODY :', data);
+        result = {
+            isSuccess: true,
+            message: 'Create Success',
+            data: data
         }
-    ];
-    bookSchema.insertMany(bookList).then((data) => {
-        console.log('data success', data);
+        res.send(result);
+    }).catch((error) => res.send(error))
+};
 
-        res.send(data);
-    })
-    // res.send(bookList);
+exports.queryBookList = (req, res) => {
+    console.log('queryBookList REQUEST_BODY :', req.body);
+    bookSchema.find().then((response) => {
+        console.log('RESPONSE_BODY :', response);
+        res.send(response);
+    }).catch((error) => res.send(error))
+};
+
+exports.queryBookById = (req, res) => {
+    console.log('queryBookById REQUEST_BODY :', req.body);
+    const queryID = {
+        _id: new ObjectID(req.body._id)
+    };
+    bookSchema.findOne(queryID).then((response) => {
+        console.log('RESPONSE_BODY :', response);
+        if (response) {
+            result = {
+                isSuccess: true,
+                message: 'Query by id success',
+                data: response
+            }
+            res.send(result);
+        } else {
+            result = {
+                isSuccess: false,
+                message: 'Data Not found ObjectID : ' + new ObjectID(req.body._id),
+                data: response
+            }
+            res.send(result);
+        }
+    }).catch((error) => res.send(error))
+};
+
+exports.updateBook = (req, res) => {
+    console.log('updateBook REQUEST_BODY :', req.body);
+    let result = {}
+    const updateID = {
+        _id: new ObjectID(req.body._id)
+    };
+    bookSchema.findOne(updateID).then((response) => {
+        if (response) {
+            bookSchema.update(updateID, req.body).then((resp) => {
+                result = {
+                    isSuccess: true,
+                    message: 'Update Success',
+                    data: req.body
+                }
+                res.send(result);
+            })
+        } else {
+            result = {
+                isSuccess: false,
+                message: 'Data Not found ObjectID : ' + new ObjectID(req.body._id),
+                data: response
+            }
+            res.send(result);
+        }
+    }).catch((error) => res.send(error))
+
+};
+
+exports.removeBookById = (req, res) => {
+    console.log('removeBook REQUEST_BODY :', req.body);
+    const removeID = {
+        _id: new ObjectID(req.body._id)
+    };
+    bookSchema.findOne(removeID).then((response) => {
+        if (response) {
+            bookSchema.deleteOne(removeID).then((resp) => {
+                result = {
+                    isSuccess: true,
+                    message: 'Remove Success',
+                    data: response
+                }
+                res.send(result);
+            })
+        } else {
+            result = {
+                isSuccess: false,
+                message: 'Data Not found ObjectID : ' + new ObjectID(req.body._id),
+                data: response
+            }
+            res.send(result);
+        }
+    }).catch((error) => res.send(error))
 };
--- src/routes/book.js
+++ src/routes/book.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/bookController');
 
-router.get('/insert', controller.getBookList);
+router.post('/createBook', controller.createBook);
+router.get('/queryBookList', controller.queryBookList);
+router.get('/queryBookById', controller.queryBookById);
+router.put('/updateBook', controller.updateBook);
+router.delete('/removeBookById', controller.removeBookById);
 
 module.exports = router;
